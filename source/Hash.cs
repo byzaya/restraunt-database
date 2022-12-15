c0f0fa03b520d80c1675b8d5253e27397636c3ad
Dushechkina Victoria <75326533+byzaya@users.noreply.github.com>
2022-12-15 18:15:55 +0300

    add

---
+++ source/Hash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KursDB
+{
+				public class Hash
+				{
+
+								public string GetHash(string input)
+								{
+												var md5 = MD5.Create();
+												var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+												return Convert.ToBase64String(hash);
+								}
+				}
+}
